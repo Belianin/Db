877bbe712bc063612549f80cea831eb20d8491f6
Belianin <belyaninwork@gmail.com>
2019-10-29 20:12:14 +0500

    A little FileLog upgrade

--- Db.Logging/FileLog.cs
+++ Db.Logging/FileLog.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,21 +10,41 @@ using Db.Logging.Abstractions;
 
 namespace Db.Logging
 {
-    [Obsolete("Two streams with buffer")]
-    public class FileLog : BaseLog
+    public class FileLog : BaseLog, IDisposable
     {
         private readonly ICollection<LogEvent> events;
+
+        private readonly CancellationTokenSource cts;
+
+        private readonly string fileName;
+        
+        private readonly TimeSpan aggregationPeriod = TimeSpan.FromMinutes(1);
         
         public FileLog(string fileName = null)
         {
             if (fileName == null)
-                fileName = $"log-{DateTime.Now.ToString("yyyy-MMM", new CultureInfo("en-US"))}.txt";
+                this.fileName = $"log-{DateTime.Now.ToString("yyyy-MMM", new CultureInfo("en-US"))}.txt";
 
             events = new List<LogEvent>();
-            var cts = new CancellationTokenSource();
-            Task.Run(() => WriteToFileTask(fileName, cts.Token));
+            cts = new CancellationTokenSource();
+            
+            Task.Run(() => WriteToFileTask(cts.Token));
         }
-        
+
+        public void Dispose()
+        {
+            cts.Cancel();
+            cts.Dispose();
+
+            if (events.Count == 0)
+                return;
+            
+            using (var textWriter = File.AppendText(fileName))
+            {
+                textWriter.Write(events.Select(s => $"{LogFormatter.Format(s)}\n"));
+            }
+        }
+
         protected override void Log(LogEvent logEvent)
         {
             lock (events)
@@ -32,12 +53,12 @@ namespace Db.Logging
             }
         }
 
-        private void WriteToFileTask(string filename, CancellationToken token)
+        private void WriteToFileTask(CancellationToken token)
         {
             var sb = new StringBuilder();
             while (!token.IsCancellationRequested)
             {
-                Thread.Sleep(1000 * 60);
+                Thread.Sleep(aggregationPeriod);
                 lock (events)
                 {
                     if (events.Count == 0)
@@ -48,7 +69,7 @@ namespace Db.Logging
                     events.Clear();
                 }
 
-                using (var textWriter = File.AppendText(filename))
+                using (var textWriter = File.AppendText(fileName))
                 {
                     textWriter.Write(sb.ToString());
                 }
